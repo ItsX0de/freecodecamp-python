f21b2c1ed9f04e22cdeda1d644a5b98d7fc6759b
X0de <seanremington1221@gmail.com>
2026-09-17 20:53:52 +0300

    Upload project 6

--- workshops/Build a Pin Extractor.py
+++ workshops/Build a Pin Extractor.py
@@ -10,7 +10,7 @@ def pin_extractor(poems):
             else:
                 secret_code += '0'
         secret_codes.append(secret_code)
-    return secret_codes
+        
 
 poem = """Stars and the moon
 shine in the sky
@@ -20,4 +20,4 @@ until the end of the night"""
 poem2 = 'The grass is green\nhere and there\nhoping for rain\nbefore it turns yellow'
 poem3 = 'There\nonce\nwas\na\ndragon'
 
-print(pin_extractor([poem, poem2, poem3]))
+# print(pin_extractor(poem))
